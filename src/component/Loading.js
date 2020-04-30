4df7c75ea608d7f8b8f7345bbb6670711fde3a4c
Roucky <Roucky>
2020-04-30 17:13:59 +0200

    changement de couleur du spinner

--- src/component/Loading.js
+++ src/component/Loading.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import  {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 const icone = <FontAwesomeIcon icon={faSpinner} size="7x" spin/>
 
 function Loading(){
 
-    return  <div>{icone}</div>;
+    return  <div className='icone'>{icone}</div>;
 }
 
 export default Loading;
