57daacf546364a1b56b26cddae98981a391cb922
Roucky <Roucky>
2020-04-30 16:02:30 +0200

    ajout du composant AjaxArtistsItem

---
+++ src/component/AJAX_ArtistItem.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ArtistItem from './ArtistItem';
+import Loading from './Loading';
+
+class AJAX_ArtistItem extends React.Component{
+    constructor(props){
+        super(props);
+        state : {
+            albums : null
+        }
+    }
+
+    componentDidMount(){
+        fetch("https://iut-info.univ-reims.fr/users/jonquet/albums/public/index.php/artistes/"+this.props.artists.id+"/albums")
+        .then((reponse) => reponse.json() )
+        .then(json => this.setState({albums : json}))
+    }
+
+    render(){
+        return this.state.albums === null ? <Loading /> : <ArtistItem artist={this.props.artist} albums={this.state.albums}/>
+    }
+}
+
+export default AJAX_ArtistItem;
